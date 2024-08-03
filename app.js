2319d788e034723bd85ed534accda249d0c7106f
devoxpk <devoxpk@gmail.com>
2024-08-03 19:43:18 +0500

    S

--- app.js
+++ app.js
@@ -1,20 +1,16 @@
 const express = require('express');
 const helmet = require('helmet');
-const cors = require('cors');
-const bodyParser = require('body-parser');
 const { ErrorResponseObject } = require('./common/http');
 const routes = require('./routes');
-const serverless = require('serverless-http');
 
 const app = express();
 
-app.use(cors());
-app.use(bodyParser.json({ limit: '50mb' }));
-app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use(helmet());
 app.use('/', routes);
 
 // default catch all handler
 app.all('*', (req, res) => res.status(404).json(new ErrorResponseObject('route not defined')));
 
-module.exports.handler = serverless(app);
+module.exports = app;
