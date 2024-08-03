134df0aa67316247b04436c805640ed3983986d7
devoxpk <devoxpk@gmail.com>
2024-08-03 19:52:15 +0500

    S

--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,8 @@ const whatsapp = require('./whatsapp.route');
 
 const r = Router();
 
-
-r.use('/whatsapp', whatsapp);
 r.use('/demo', demo);
+r.use('/whatsapp', whatsapp);
 
 r.get('/', (req, res) => res.json(new SuccessResponseObject('express vercel boiler plate')));
 
